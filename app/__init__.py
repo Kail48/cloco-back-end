f3d2a169f68e25b3c831d6b6dfae7b5b0b2c091d
kail48 <kailash48rai@gmail.com>
2023-08-06 15:19:13 +0545

    added config for jwt setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask 
-
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
 app=Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "cloco-code"  #I have used simple secret key and directly instead of fetching from env variable
+
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=3)
+jwt = JWTManager(app)
 class Database:
     name="cloco_db"
 #place import statements for modules within this packege to avoid circular import
