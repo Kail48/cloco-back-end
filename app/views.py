93a35282de028b8ce2b4b226a55257666a145d54
kail48 <kailash48rai@gmail.com>
2023-08-06 15:36:55 +0545

    added a protected route foradmins to create a new user

--- app/views.py
+++ app/views.py
@@ -126,3 +126,84 @@ def protected():
     if user is None:
         return jsonify(error_message="please login again"), 400
     return jsonify(is_admin=True if user[5] == True else False), 200
+
+@app.route("/user", methods=["POST"])
+@jwt_required()
+def register_user():
+    current_user_email = get_jwt_identity()
+    query = "SELECT * FROM user WHERE email=?"
+    param = (current_user_email,)
+    user = db_get_one(query=query, param=param)
+    if user is None:
+        return jsonify(error_message="please login again")
+    if user[5] == False:  # check if the user is admin
+        return jsonify(error_message="admin privilege required"), 401
+    data = request.get_json()
+
+    # check if all required fields are present
+    if data["email"] is None:
+        message = {"error_message": "email is required"}
+        return jsonify(message), 400
+    if data["password"] is None:
+        message = {"error_message": "password is required"}
+        return jsonify(message), 400
+    if data["password2"] is None:
+        message = {"error_message": "password2 is required"}
+        return jsonify(message), 400
+    if data["first_name"] is None:
+        message = {"error_message": "first name is required"}
+        return jsonify(message), 400
+    if data["last_name"] is None:
+        message = {"error_message": "last name is required"}
+        return jsonify(message), 400
+    if data["address"] is None:
+        message = {"error_message": "address is required"}
+        return jsonify(message), 400
+    if data["dob"] is None:
+        message = {"error_message": "date of birth is required"}
+        return jsonify(message), 400
+    if data["phone"] is None:
+        message = {"error_message": "phone is required"}
+        return jsonify(message), 400
+    if data["gender"] is None:
+        message = {"error_message": "gender is required"}
+        return jsonify(message), 400
+
+    # validate special form datas
+    if validate_email(data["email"]) == False:
+        message = {"error_message": "enter a valid email address"}
+        return jsonify(message), 400
+    if validate_unique_email(email=data["email"]) == False:
+        message = {"error_message": "This email is already in use"}
+        return jsonify(message), 400
+    if validate_password(data["password"]) == False:
+        message = {
+            "error_message": "pasword must contain an uppercase letter, a digit and at least be 8 characters long"
+        }
+        return jsonify(message), 400
+    if validate_password_match(data["password"], data["password2"]) == False:
+        message = {"error_message": "passwords do not match"}
+        return jsonify(message), 400
+    bcrypt = Bcrypt(app)  # for password hashing
+    hashed_password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+    created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    query = """INSERT INTO user(first_name, last_name, email, password, phone, dob, gender, address, created_at, updated_at, is_admin)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?);"""
+    insert_data = (
+        data["first_name"],
+        data["last_name"],
+        data["email"],
+        hashed_password,
+        data["phone"],
+        data["dob"],
+        data["gender"],
+        data["address"],
+        created_at,
+        None,
+        0,
+    )
+    execute_query = db_insert_one(query=query, insert_data=insert_data)
+    if execute_query:
+        return "done"
+    else:
+        return jsonify({"error_message": "something went wrong"}), 500
