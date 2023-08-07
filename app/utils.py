716253932411885f8e10b66abc54128fc87d1228
kail48 <kailash48rai@gmail.com>
2023-08-07 17:17:28 +0545

    fixed bugs related to syntax error

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 from .db_queries import db_get_one
+from .validators import is_year,is_valid_date
 import csv,os
 from werkzeug.utils import secure_filename
 from app import app
@@ -21,18 +22,31 @@ def artist_csv_to_dictionary(file):
     filename = secure_filename(file.filename) #save the file with secure name
     filepath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(filepath)
-    d = {}
     field_names=["name","dob","gender","address","first_release_year","number_of_albums_released"]
     with open(filepath, mode='r') as f:
         reader = csv.DictReader(f)
         artist_list=[]
+        #check for invalid data
         for row in reader:
             artist_dictionary={}
+            #check if any data field is empty
+            for key in row.keys():
+                if row[key] is None:
+                    return None
+            #check if the release year is valid
+            if is_year(row["first_release_year"])==False:
+                return None
+            #check if the date is in valid date format
+            if is_valid_date(row["dob"])==False:
+                return None
+            #create a dictioanry from field list and append to the artist list
             for field_name in field_names:
                 artist_dictionary[field_name]=row[field_name]
-            artist_list.append(artist_dictionary)
+            artist_list.append(artist_dictionary)   
+       
+
         print(artist_list)
     
     #delete the file from server after operation is complete
     os.remove(filepath)
-    return d
+    return artist_list
--- app/validators.py
+++ app/validators.py
@@ -1,4 +1,5 @@
-import sqlite3,pathlib,re
+import pathlib,re
+from datetime import datetime
 from .db_queries import db_get_one
 
 def validate_password_match(password1,password2):
@@ -99,4 +100,9 @@ def is_year(data):
         return False
     return True
 
-        
+def is_valid_date(date_string):
+    try:
+        datetime.strptime(date_string,'%Y-%m-%d')
+        return True
+    except ValueError:
+        return False   
