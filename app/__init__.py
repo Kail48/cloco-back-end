76f63ed6efe29330863d79f72b9cbc6d22946d65
kail48 <kailash48rai@gmail.com>
2023-08-07 22:44:34 +0545

    allowed cors to connect from all apps

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask 
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-
+from flask_cors import CORS
 
 UPLOAD_FOLDER = '/home/kuroashi/cloco/cloco-back-end/app/uploaded_files' #path to save uploaded files
 ALLOWED_EXTENSIONS = {'.csv'}
 app=Flask(__name__)
+CORS(app)
 app.config["JWT_SECRET_KEY"] = "cloco-code"  #I have used simple secret key and directly instead of fetching from env variable
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=3)
