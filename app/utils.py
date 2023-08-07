8f0bda8089eedd3ab892545b656b266f5acf63bc
kail48 <kailash48rai@gmail.com>
2023-08-07 15:21:02 +0545

    made csv to dictionary specific to artist data

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 from .db_queries import db_get_one
-
+import csv,os
+from werkzeug.utils import secure_filename
+from app import app
 def user_exists(id):
     query = "SELECT * FROM user WHERE id=?"
     param = (id,)
@@ -14,3 +16,23 @@ def artist_exists(id):
     if user is None:
         return False
     return True
+#receives a filestorage object and returns dictionary
+def artist_csv_to_dictionary(file):
+    filename = secure_filename(file.filename) #save the file with secure name
+    filepath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    d = {}
+    field_names=["name","dob","gender","address","first_release_year","number_of_albums_released"]
+    with open(filepath, mode='r') as f:
+        reader = csv.DictReader(f)
+        artist_list=[]
+        for row in reader:
+            artist_dictionary={}
+            for field_name in field_names:
+                artist_dictionary[field_name]=row[field_name]
+            artist_list.append(artist_dictionary)
+        print(artist_list)
+    
+    #delete the file from server after operation is complete
+    os.remove(filepath)
+    return d
